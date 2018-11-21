cdc7e64b561c688e03e75b817f154d2fd15b9b8d
Aaron Tillekeratne <aaron.tillekeratne@nab.com.au>
2018-11-21 20:40:33 +1100

    P07-HOTFIX: Removed incorrect package def

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='plotly_helper', version='0.01',
-    packages=['plotly', 'test', 'test.unit', 'test.unit.notebook', 'service',
+    packages=['', 'test', 'test.unit', 'test.unit.notebook', 'service',
               'plotting', 'abstraction'],
     url='https://github.com/codeBehindMe/plotly_helper', license='Apache 2.0',
     author='Machine  Sciences Lab',
