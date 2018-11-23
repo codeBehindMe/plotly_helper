f291885df6d072856ec93fdf3847be990e331847
Aaron Tillekeratne <aaron.tillekeratne@nab.com.au>
2018-11-23 19:16:57 +1100

    P09: Created bar chart shell class

---
+++ plotting/bar_plot.py
@@ -0,0 +1,16 @@
+from abstraction.meta import Chart
+
+
+
+class BarChart(Chart):
+    def __add__(self, other):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        pass
+
+    def get_plotly_object(self):
+        pass
+
+    def _prepare_plot_package(self):
+        pass
