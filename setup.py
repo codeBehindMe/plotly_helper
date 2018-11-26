c962bb48bc56b03d5c463a5f1f87df81ce3b6b66
Aaron Tillekeratne <aaron.tillekeratne@nab.com.au>
2018-11-26 18:58:56 +1100

    defined package dir

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='plotly_helper', version='0.01',
-    packages=['', 'test', 'test.unit', 'test.unit.plotting', 'service',
-              'plotting', 'abstraction'],
-    url='https://github.com/codeBehindMe/plotly_helper', license='Apache 2.0',
-    author='Machine  Sciences Lab',
-    author_email='aaron.tillekeratne@gmail.com',
-    description='Quick helper for plotly.')
+      packages=['', 'test', 'test.unit', 'test.unit.plotting', 'service',
+                'plotting', 'abstraction'],
+      url='https://github.com/codeBehindMe/plotly_helper',
+      license='Apache 2.0', author='Machine  Sciences Lab',
+      author_email='aaron.tillekeratne@gmail.com',
+      description='Quick helper for plotly.', package_dir={'': 'lib'})
