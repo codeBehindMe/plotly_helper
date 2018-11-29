041fac54dab40a709a3ba46ac733912c4b7d85f7
Aaron Tillekeratne <aaron.tillekeratne@nab.com.au>
2018-11-29 16:50:36 +1100

    HOTFIX: Updated setup

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
-setup(name='plotly_helper', version='',
+setup(name='plotly_helper', version='0.1',
     packages=['test', 'test.unit', 'test.unit.plotting', 'service',
-              'plotting', 'abstraction'], url='', license='',
-    author='Machine  Sciences Lab', author_email='', description='')
+              'plotting', 'abstraction'],
+    url='https://github.com/codeBehindMe/plotly_helper', license='MIT',
+    author='Machine  Sciences Lab',
+    author_email='aaron.tillekeratne@gmail.com',
+    description='Quick plotly helper.')
